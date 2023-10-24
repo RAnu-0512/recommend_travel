7dd172adde5070a9dd0a9cbbf9459dec48048f44
ranu <fkddn123@yahoo.co.jp>
2023-10-24 19:51:46 +0900

    get lat and lng

--- for_interface.js
+++ for_interface.js
@@ -1,17 +1,27 @@
 // 六本木を中心に地図描画
+var lat0=34.66640; 
+var lng0=133.919066; 
+//34.685028, 133.927567
 
-var map = L.map('mapid', {
-    center: [34.685028, 133.927567],
-    zoom: 17,
+var mymap = L.map('mapid', {
+    center: [lat0, lng0],
+    zoom: 16,
   }); 
+
+// OpenStreetMap から地図画像を読み込む
+var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+});
+tileLayer.addTo(mymap);
   
-  // OpenStreetMap から地図画像を読み込む
-  
-  var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-  });
-  tileLayer.addTo(map);
+
+function onMapClick(e){
+  //クリック位置経緯度取得
+  lat = e.latlng.lat;
+  lng = e.latlng.lng;
   
   // マーカー画像の場所を指定する
-  
-  L.marker([34.685028, 133.927567]).addTo(map); 
+  L.marker([lat, lng]).addTo(mymap).bindPopup("開始位置").openPopup(); 
+}
+mymap.on('click', onMapClick)
+
